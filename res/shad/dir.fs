8044155c061c3831835ac9e4fcdf0aca931693dc
Jack <jackhasakeyboard@gmail.com>
2022-12-19 21:58:53 -0700

    Brought down intensity of lighting

--- res/shad/dir.fs
+++ res/shad/dir.fs
@@ -11,7 +11,7 @@ vec3 sun = vec3(1.0, 1.0, -1.0);
 void main() {
 	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
 
-	float diff = max(dot(normFace, normalize(sun)), 0.0);
+	float diff = max(dot(normFace, normalize(sun)), 0.0) * 0.2;
 
 	frag = vec4((1.0 - diff) * vec3(0.8381, 0.8441, 0.5802), 1.0);
 }
