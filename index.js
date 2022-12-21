12bdd235f7b8b4f3efc59be2165461f622b2bf9f
Jack <jackhasakeyboard@gmail.com>
2022-12-20 19:50:18 -0700

    Created secondary mesh class parallel to the first one loading from a file, not one the same with the first one because of the lack of support for multiple constructors and the complexity with implementing as a parent class. Transforming via mouse events currently non-functional

--- index.js
+++ index.js
@@ -99,115 +99,17 @@ document.addEventListener('DOMContentLoaded', function() {
 		alert('Your browser does not support WebGL');
 	}
 
-	var scr = new Mesh(scrVtc, scrIdc, "scr", "solid");
-
-	var vao = window.gl.createVertexArray();
-	window.gl.bindVertexArray(vao);
-
-	// Positions
-	var vbo = window.gl.createBuffer();
-	window.gl.bindBuffer(window.gl.ARRAY_BUFFER, vbo);
-
-	var vtc = Ld.vtc('cabinet');
-	window.gl.bufferData(window.gl.ARRAY_BUFFER, new Float32Array(vtc), window.gl.STATIC_DRAW);
-
-	// Indices
-	var ibo = window.gl.createBuffer();
-	window.gl.bindBuffer(window.gl.ELEMENT_ARRAY_BUFFER, ibo);
-
-	var idc = Ld.idc('cabinet', type.VTX);
-	window.gl.bufferData(window.gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(idc), window.gl.STATIC_DRAW);
-
-	// Matrices
-	mat4.identity(model);
-
-	mat4.lookAt(
-		view,
-		[
-			15, 8, 0
-		], [
-			-0.5846, 2.7, 0
-		], [
-			0, 1, 0
-		]
-	);
-	mat4.perspective(proj, (1 / 4) * Math.PI, window.canv.clientWidth / window.canv.clientHeight, 0.1, 1000.0);
-
-	mat4.identity(id);
-
-	mat4.rotate(rot, id, theta, [0, 1, 0]);
-	mat4.mul(model, rot, id);
-
 	window.gl.enable(window.gl.DEPTH_TEST);
 
-	// Shader
-	const
-		shadVtxBuff = Util.rd('res/shad/obj.vs'),
-		shadFragBuff = Util.rd('res/shad/dir.fs');
+	var cabinet = new MeshLd('cabinet', 'obj', 'dir');
 
-	// Vertex
-	var shadVtx = window.gl.createShader(window.gl.VERTEX_SHADER);
-	window.gl.shaderSource(shadVtx, shadVtxBuff);
-
-	window.gl.compileShader(shadVtx);
-	if (!window.gl.getShaderParameter(shadVtx, window.gl.COMPILE_STATUS)) {
-		console.error('Error compiling vertex shader', window.gl.getShaderInfoLog(shadVtx));
-	}
-
-	// Fragment
-	var shadFrag = window.gl.createShader(window.gl.FRAGMENT_SHADER);
-	window.gl.shaderSource(shadFrag, shadFragBuff);
-
-	window.gl.compileShader(shadFrag);
-	if (!window.gl.getShaderParameter(shadFrag, window.gl.COMPILE_STATUS)) {
-		console.error('Error compiling fragment shader', window.gl.getShaderInfoLog(shadFrag));
-	}
-
-	// Program
-	prog = window.gl.createProgram();
-
-	window.gl.attachShader(prog, shadVtx);
-	window.gl.attachShader(prog, shadFrag);
-
-	window.gl.linkProgram(prog);
-	if (!window.gl.getProgramParameter(prog, window.gl.LINK_STATUS)) {
-		console.error('Error linking program', window.gl.getProgramInfoLog(prog));
-	}
-
-	window.gl.validateProgram(prog);
-	if (!window.gl.getProgramParameter(prog, window.gl.VALIDATE_STATUS)) {
-		console.error('Error validating program', window.gl.getProgramInfoLog(prog));
-	}
-
-	window.gl.useProgram(prog);
-
-	// Attributes
-	var attrPos = window.gl.getAttribLocation(prog, 'pos');
-	window.gl.vertexAttribPointer(attrPos, 3, window.gl.FLOAT, window.gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0);
-	window.gl.enableVertexAttribArray(attrPos);
-
-	// Uniforms
-	uniModel = window.gl.getUniformLocation(prog, 'model');
-	uniView = window.gl.getUniformLocation(prog, 'view');
-	uniProj = window.gl.getUniformLocation(prog, 'proj');
-
-	window.gl.uniformMatrix4fv(uniModel, window.gl.FALSE, model);
-	window.gl.uniformMatrix4fv(uniView, window.gl.FALSE, view);
-	window.gl.uniformMatrix4fv(uniProj, window.gl.FALSE, proj);
-
-	window.gl.useProgram(null);
+	var scr = new Mesh(scrVtc, scrIdc, "scr", "solid");
 
 	function draw() {
 		window.gl.clearColor(1 - ((1 - (col[0] / 255)) / 2), 1 - ((1 - (col[1] / 255)) / 2), 1 - ((1 - (col[2] / 255)) / 2), 1);
 		window.gl.clear(window.gl.DEPTH_BUFFER_BIT | window.gl.COLOR_BUFFER_BIT);
 
-		window.gl.bindVertexArray(vao);
-		window.gl.useProgram(prog);
-
-		window.gl.drawElements(window.gl.TRIANGLES, idc.length, window.gl.UNSIGNED_BYTE, 0);
-
-		window.gl.useProgram(null);
-		window.gl.bindVertexArray(null);
+		cabinet.draw();
 
 		scr.draw();
 
