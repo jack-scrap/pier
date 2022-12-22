0e47e0bc6c41985b735bc3f91283d0c6b217c06d
Jack <jackhasakeyboard@gmail.com>
2022-12-21 19:40:01 -0700

    Offset screen at the proper location

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	window.gl.enable(window.gl.CULL_FACE);
 	gl.cullFace(gl.BACK);
 
-	scr = new MeshLd('scr', 'scr', 'solid');
+	scr = new MeshLd('scr', 'scr', 'solid', scrLoc);
 
 	cabinet = new MeshLd('cabinet', 'obj', 'dir', [0, 0, 0], [0, theta, 0], [
 		scr
