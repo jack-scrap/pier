8ca6d520100d4df2d9ae82c2de275f3fa59cfc87
Jack <jackhasakeyboard@gmail.com>
2022-12-23 00:04:39 -0700

    Formatted

--- index.js
+++ index.js
@@ -84,65 +84,65 @@ document.addEventListener('DOMContentLoaded', function() {
 	ctx.useProgram(scr.prog.id);
 
 	/* Triangle */
-	let vaoTri = ctx.createVertexArray();
-	ctx.bindVertexArray(vaoTri);
+	let vaoShip = ctx.createVertexArray();
+	ctx.bindVertexArray(vaoShip);
 
-	const vtcTri = [
+	const vtcShip = [
 		-0.6, -1.0,
 		0.6, -1.0,
 		0.0, 1.0
 	];
 
-	let vboTri = ctx.createBuffer();
-	ctx.bindBuffer(ctx.ARRAY_BUFFER, vboTri);
-	ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(vtcTri), ctx.STATIC_DRAW);
+	let vboShip = ctx.createBuffer();
+	ctx.bindBuffer(ctx.ARRAY_BUFFER, vboShip);
+	ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(vtcShip), ctx.STATIC_DRAW);
 
-	const shadVtxTriBuff = Util.rd('res/shad/scr_spc.vs');
+	const shadVtxShipBuff = Util.rd('res/shad/scr_spc.vs');
 
-	let shadVtxTri = ctx.createShader(ctx.VERTEX_SHADER);
-	ctx.shaderSource(shadVtxTri, shadVtxTriBuff);
+	let shadVtxShip = ctx.createShader(ctx.VERTEX_SHADER);
+	ctx.shaderSource(shadVtxShip, shadVtxShipBuff);
 
-	const shadFragTriBuff = Util.rd('res/shad/green.fs');
+	const shadFragShipBuff = Util.rd('res/shad/green.fs');
 
-	let shadFragTri = ctx.createShader(ctx.FRAGMENT_SHADER);
-	ctx.shaderSource(shadFragTri, shadFragTriBuff);
+	let shadFragShip = ctx.createShader(ctx.FRAGMENT_SHADER);
+	ctx.shaderSource(shadFragShip, shadFragShipBuff);
 
-	ctx.compileShader(shadVtxTri);
-	if (!ctx.getShaderParameter(shadVtxTri, ctx.COMPILE_STATUS)) {
-		console.error('Vertex error: ', ctx.getShaderInfoLog(shadVtxTri));
+	ctx.compileShader(shadVtxShip);
+	if (!ctx.getShaderParameter(shadVtxShip, ctx.COMPILE_STATUS)) {
+		console.error('Vertex error: ', ctx.getShaderInfoLog(shadVtxShip));
 	}
 
-	ctx.compileShader(shadFragTri);
-	if (!ctx.getShaderParameter(shadFragTri, ctx.COMPILE_STATUS)) {
-		console.error('Fragment error: ', ctx.getShaderInfoLog(shadFragTri));
+	ctx.compileShader(shadFragShip);
+	if (!ctx.getShaderParameter(shadFragShip, ctx.COMPILE_STATUS)) {
+		console.error('Fragment error: ', ctx.getShaderInfoLog(shadFragShip));
 	}
 
-	let progTri = ctx.createProgram();
-	ctx.attachShader(progTri, shadVtxTri);
-	ctx.attachShader(progTri, shadFragTri);
+	let progShip = ctx.createProgram();
+	ctx.attachShader(progShip, shadVtxShip);
+	ctx.attachShader(progShip, shadFragShip);
 
-	ctx.linkProgram(progTri);
-	if (!ctx.getProgramParameter(progTri, ctx.LINK_STATUS)) {
+	ctx.linkProgram(progShip);
+	if (!ctx.getProgramParameter(progShip, ctx.LINK_STATUS)) {
 		console.error('Error linking program', ctx.getProgramInfoLog(prog));
 	}
 
-	ctx.validateProgram(progTri);
-	if (!ctx.getProgramParameter(progTri, ctx.VALIDATE_STATUS)) {
+	ctx.validateProgram(progShip);
+	if (!ctx.getProgramParameter(progShip, ctx.VALIDATE_STATUS)) {
 		console.error('Error validating program', ctx.getProgramInfoLog(prog));
 	}
 
-	let modelTri = new Float32Array(16);
-	mat4.identity(modelTri);
+	let modelShip = new Float32Array(16);
+	mat4.identity(modelShip);
 
-	ctx.useProgram(progTri);
+	ctx.useProgram(progShip);
 
 	// Attributes
-	let attrPosTri = ctx.getAttribLocation(progTri, 'pos');
-	ctx.vertexAttribPointer(attrPosTri, 2, ctx.FLOAT, ctx.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
-	ctx.enableVertexAttribArray(attrPosTri);
+	let attrPosShip = ctx.getAttribLocation(progShip, 'pos');
+	ctx.vertexAttribPointer(attrPosShip, 2, ctx.FLOAT, ctx.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+	ctx.enableVertexAttribArray(attrPosShip);
 
-	let uniModelTri = ctx.getUniformLocation(progTri, 'model');
-	ctx.uniformMatrix4fv(uniModelTri, ctx.FALSE, modelTri);
+	let uniModelShip = ctx.getUniformLocation(progShip, 'model');
+	ctx.uniformMatrix4fv(uniModelShip, ctx.FALSE, modelShip);
 
 	ctx.useProgram(null);
 	ctx.bindVertexArray(null);
@@ -244,11 +244,11 @@ document.addEventListener('DOMContentLoaded', function() {
 		ctx.clearColor(0, 0.06, 0, 1.0);
 		ctx.clear(ctx.COLOR_BUFFER_BIT);
 
-		ctx.bindVertexArray(vaoTri);
-		ctx.useProgram(progTri);
+		ctx.bindVertexArray(vaoShip);
+		ctx.useProgram(progShip);
 
-		mat4.translate(modelTri, modelTri, [0, 0.003, 0]);
-		ctx.uniformMatrix4fv(uniModelTri, ctx.FALSE, modelTri);
+		mat4.translate(modelShip, modelShip, [0, 0.003, 0]);
+		ctx.uniformMatrix4fv(uniModelShip, ctx.FALSE, modelShip);
 
 		ctx.drawArrays(ctx.LINE_LOOP, 0, 3);
 
