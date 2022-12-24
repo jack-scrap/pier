ac5f9e9957ab861a10f23f1706d3573b5c3e13c0
Jack <jackhasakeyboard@gmail.com>
2022-12-23 19:35:55 -0700

    Created unused mesh class, has no index buffer

---
+++ mesh.js
@@ -0,0 +1,16 @@
+class Mesh {
+	vao;
+
+	_vbo;
+
+	constructor(vtc) {
+		this.vao = gl.createVertexArray();
+		gl.bindVertexArray(this.vao);
+
+		this._vbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this._vbo);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vtc), gl.STATIC_DRAW);
+
+		gl.bindVertexArray(null);
+	}
+};
