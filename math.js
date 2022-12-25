556662bd32d8ef67711a42392c4240cc1de51cd2
Jack <jackhasakeyboard@gmail.com>
2022-12-24 17:26:07 -0700

    Added parallel math function generating random floating-point value

--- math.js
+++ math.js
@@ -3,3 +3,7 @@ const axes = 3;
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function randFloat(min, max) {
+	return Math.random() * (max - min) + min;
+}
