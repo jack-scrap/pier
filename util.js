28f3ed9e9aae70073601f6d4ce77fa4bfb0965c5
Jack <jackhasakeyboard@gmail.com>
2022-12-24 20:22:38 -0700

    Updated utility testing SAT collision to evaluate proper member

--- util.js
+++ util.js
@@ -112,7 +112,7 @@ class Phys {
 
 			let model = [];
 			for (let i = 0; i < 2; i++) {
-				model[i] = apply(poly[i].vtc, poly[i].model);
+				model[i] = apply(poly[i]._pt, poly[i].model);
 			}
 
 			for (let v = 0; v < poly[0].n; v++) {
