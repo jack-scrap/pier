2dcc2565ed7131316ffeeb29aa129abe6af2675f
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2024-01-12 19:43:09 -0700

    Drew triangle, re-included all scripts and temporarily disabled face culling causing a single triangle to be culled

--- public/res/shad/vec.vs
+++ public/res/shad/vec.vs
@@ -4,14 +4,6 @@ precision mediump float;
 
 in vec2 pos;
 
-uniform mat4 model;
-
-const float scale = 0.1;
-
-const vec2 dim = vec2(1.2, 0.6);
-
 void main() {
-	const float aspect = dim[0] / dim[1];
-
-  gl_Position = model * vec4(vec3(vec2(pos.x / aspect, pos.y) * scale, 0.0), 1.0);
+  gl_Position = vec4(vec3(pos, 0.0), 1.0);
 }
--- public/script/index.js
+++ public/script/index.js
@@ -15,12 +15,41 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 	gl.enable(gl.DEPTH_TEST);
 
-	gl.enable(gl.CULL_FACE);
-
 	gl.clearColor(1, 0, 0, 1);
+
+	const pt = [
+		0.0, 0.0,
+		0.5, 1.0,
+		1.0, 0.0
+	];
+
+	let vao = gl.createVertexArray();
+	gl.bindVertexArray(vao);
+
+	let vbo = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pt), gl.STATIC_DRAW);
+
+	let prog = new Prog("vec", "green");
+
+	prog.use();
+
+	let attrPos = gl.getAttribLocation(prog.id, "pos");
+	gl.vertexAttribPointer(attrPos, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.enableVertexAttribArray(attrPos);
+
+	prog.unUse();
 
 	function draw() {
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		gl.bindVertexArray(vao);
+		prog.use();
+
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+		prog.unUse();
+		gl.bindVertexArray(null);
 
 		requestAnimationFrame(draw);
 	}
