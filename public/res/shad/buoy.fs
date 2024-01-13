d7ff2f822fed08770974e0d1d2e7c9a21f05ea38
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2024-01-12 23:20:45 -0700

    Updated buoy shader

--- public/res/shad/buoy.fs
+++ public/res/shad/buoy.fs
@@ -5,8 +5,12 @@ precision mediump float;
 in vec3 _pos;
 out vec4 frag;
 
-const float stride = 1.0;
+const float stroke = 0.16;
+
+bool inRng(float val, float floor, float roof) {
+	return val >= floor && val <= roof;
+}
 
 void main() {
-	frag = vec4(vec3(mod(_pos.x, stride) > stride / 2.0 ? vec3(1.0, 0.0, 0.0) : vec3(1.0)), 1.0);
+	frag = vec4(inRng(_pos.x, -stroke, stroke) ||  inRng(_pos.z, -stroke, stroke) ? vec3(1.0) : vec3(1.0, 0.0, 0.0), 1.0);
 }
