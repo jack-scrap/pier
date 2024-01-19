d6ffd9239e3efb2a898f77062fbcbfeedb8889b1
Jack <jackthagamerhd@gmail.com>
2024-01-18 23:42:22 -0700

    Created declarations for colors

--- public/res/shad/wood.fs
+++ public/res/shad/wood.fs
@@ -8,6 +8,10 @@ out vec4 frag;
 
 const float pi = 3.141593;
 
+const vec3 light = vec3(225, 195, 167);
+
+const vec3 saturated = vec3(225, 170, 119);
+
 bool hex(vec2 loc, float rad) {
 	loc = loc * 2.0 - 1.0;
 
@@ -19,15 +23,15 @@ bool hex(vec2 loc, float rad) {
 }
 
 void main() {
-	frag = vec4(vec3(225, 195, 167) / 255.0, 1.0);
+	frag = vec4(light / 255.0, 1.0);
 
 	const int n = 7;
 	for (int i = 0; i < n; i++) {
 		if (!hex(_pos.xy, float(n - i))) {
 			if (bool(mod(float(i), 2.0))) {
-				frag = vec4(vec3(225, 195, 167) / 255.0, 1.0);
+				frag = vec4(saturated / 255.0, 1.0);
 			} else {
-				frag = vec4(vec3(225, 170, 119) / 255.0, 1.0);
+				frag = vec4(light / 255.0, 1.0);
 			}
 		}
 	}
