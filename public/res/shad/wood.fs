55a1d36e4c7df20d9d67f49ad7d2377edf295314
Jack <jackthagamerhd@gmail.com>
2024-01-18 22:18:33 -0700

    Replaced circle with a polygon, not distributed from the origin in the same way

--- public/res/shad/wood.fs
+++ public/res/shad/wood.fs
@@ -6,10 +6,18 @@ in vec2 _pos;
 
 out vec4 frag;
 
-bool circ(float rad, vec2 loc) {
-	return bool(step(rad, distance(loc, vec2(0.0))));
+const float pi = 3.141593;
+
+bool hex(vec2 loc, float rad) {
+	loc = loc * 2.0 - 1.0;
+
+	float angle = atan(loc.x, loc.y);
+
+	float slice = pi * 2.0 / float(6);
+
+	return bool(step(rad, cos(floor(0.5 + angle / slice) * slice - angle) * length(loc)));
 }
 
 void main() {
-	frag = vec4((circ(1.0, _pos) ? vec3(225, 195, 167) : vec3(225, 170, 119)) / 255.0, 1.0);
+	frag = vec4(vec3((hex(_pos.xy, 1.0) ? vec3(225, 195, 167) : vec3(225, 170, 119)) / vec3(255.0)), 1.0);
 }
