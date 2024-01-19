0673adeac3e5750051517f8ed8f2d46277946915
Jack <jackthagamerhd@gmail.com>
2024-01-18 21:42:59 -0700

    Removed continuous transformation on plank

--- public/script/index.js
+++ public/script/index.js
@@ -79,9 +79,6 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 	let t = 0;
 	function draw() {
-		mat4.identity(plank.model);
-		mat4.translate(plank.model, plank.model, [0, Math.sin(t / 100) * amp, 0]);
-
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 		plank.prog.use();
