1f3265fcca58e0b2114601f009875a8968f6dd09
Jack <jackthagamerhd@gmail.com>
2024-01-19 22:33:49 -0700

    Converged color declarations and indexed based on the binary operation of the modulating shader

--- public/res/shad/wood.fs
+++ public/res/shad/wood.fs
@@ -8,9 +8,10 @@ out vec4 frag;
 
 const float pi = 3.141593;
 
-const vec3 light = vec3(225, 195, 167);
-
-const vec3 saturated = vec3(225, 170, 119);
+const vec3 col[2] = vec3[2](
+	vec3(225, 195, 167),
+	vec3(225, 170, 119)
+);
 
 bool hex(vec2 loc, float rad) {
 	loc = loc * 2.0 - 1.0;
@@ -23,16 +24,12 @@ bool hex(vec2 loc, float rad) {
 }
 
 void main() {
-	frag = vec4(light / 255.0, 1.0);
+	frag = vec4(col[int(true)] / 255.0, 1.0);
 
 	const int n = 7;
 	for (int i = 0; i < n; i++) {
 		if (hex(_pos.xy, float(n - i))) {
-			if (bool(mod(float(i), 2.0))) {
-				frag = vec4(saturated / 255.0, 1.0);
-			} else {
-				frag = vec4(light / 255.0, 1.0);
-			}
+			frag = vec4(col[int(bool(mod(float(i), 2.0)))] / 255.0, 1.0);
 		}
 	}
 }
